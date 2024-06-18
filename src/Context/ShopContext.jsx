317f95c2b27fa67133ec0041f47552d5f07cd982
ducct <lavanduc143@gmail.com>
2024-06-18 18:28:01 +0700

    change local deploy

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -17,12 +17,12 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart())
 
     useEffect(() => {
-        fetch('http://localhost:4000/allproduct')
+        fetch('https://ecommer-backend-ten.vercel.app/allproduct')
         .then((response) => response.json())
         .then((data) => setAll_Product(data))
 
         if(localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4000/getcart', {
+            fetch('https://ecommer-backend-ten.vercel.app/getcart', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/form-data',
@@ -39,7 +39,7 @@ const ShopContextProvider = (props) => {
     const addToCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1}))
         if(localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4000/addtocart', {
+            fetch('https://ecommer-backend-ten.vercel.app/addtocart', {
                 method:'POST',
                 headers: {
                     Accept:'application/form-data',
@@ -56,7 +56,7 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1}))
         if(localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4000/removefromcart', {
+            fetch('https://ecommer-backend-ten.vercel.app/removefromcart', {
                 method:'POST',
                 headers: {
                     Accept:'application/form-data',
